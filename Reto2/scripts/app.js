4491b5cc62459b571e483de7e82d7662d78b0302
LuisSanchez851 <139244405+LuisSanchez851@users.noreply.github.com>
2023-09-07 22:59:56 -0500

    Add files via upload

---
+++ Reto2/scripts/app.js
@@ -0,0 +1,6 @@
+'use strict';
+const content_imagen = document.getElementById("content_imagen");
+
+content_imagen.addEventListener("click", () =>{
+    content_imagen.classList.toggle("con-borde");
+});
